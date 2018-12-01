896dc6aab0e888ed9ca3986b9ffb5298d279049c
TURCHINI GUILLAUME <orion78fr@gmail.com>
2018-12-01 12:30:20 +0100

    Day 1 part 2

--- year2018/day1/src/main.rs
+++ year2018/day1/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
     let file_content = fs::read_to_string("input").unwrap();
@@ -12,8 +13,30 @@ fn main() {
         });
 
     println!("Frequency is {}", frequency);
+
+    let dupe = find_first_dupe_freq(file_content);
+    println!("First duplicated frequency is {}", dupe)
 }
 
-fn parse_freq_change(freq_change : &str) -> i32 {
+fn find_first_dupe_freq(file_content: String) -> i32 {
+    let mut set = HashSet::new();
+    let mut frequency = 0;
+    set.insert(0);
+
+    loop {
+        for change in file_content.lines() {
+            let change = parse_freq_change(change);
+            frequency += change;
+
+            if set.contains(&frequency) {
+                return frequency;
+            } else {
+                set.insert(frequency);
+            }
+        }
+    }
+}
+
+fn parse_freq_change(freq_change: &str) -> i32 {
     freq_change.parse().unwrap()
 }
