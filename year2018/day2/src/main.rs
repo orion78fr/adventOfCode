ddac954eb74eafac66b30e8a054ce65ac50b410e
TURCHINI GUILLAUME <orion78fr@gmail.com>
2018-12-03 00:28:53 +0100

    Stash

--- year2018/day2/src/main.rs
+++ year2018/day2/src/main.rs
@@ -6,6 +6,7 @@ const PUZZLE: &str = include_str!("../input");
 fn main() {
     let mut match2 = 0;
     let mut match3 = 0;
+    let mut occurrences = HashMap::new();
 
     PUZZLE.lines().for_each(|id| {
         let mut letter_occurrences = HashMap::new();
@@ -23,8 +24,14 @@ fn main() {
         if letter_occurrences.iter().any(|(_, v)| *v == 3) {
             match3 += 1;
         }
+
+        occurrences.insert(id, letter_occurrences);
     });
 
     // For my input : 4920
     println!("Checksum : {}", match2 * match3);
+
+    let mut best_match : Option<(i32, i32, i32)> = None();
+
+
 }
